1b43ed64921d49149fd8dc5ded5f40af89fbb167
asw-sq <58130503@qq.com>
2020-09-22 12:31:04 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/msite">导航1</router-link> 
+      <router-link to="/order">导航2</router-link> 
+      <router-link to="/profile">导航3</router-link> 
+      <router-link to="/search">导航4</router-link> 
     <router-view/>
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>
---
+++ src/components/FooterNav/FooterNav.vue
@@ -0,0 +1,23 @@
+<!-- 1、创建组件 -->
+<template>
+  <div>
+    <!-- 挂载DOM -->
+    <router-link to="/msite"></router-link>
+    <router-link to="/order"></router-link>
+    <router-link to="/profile"></router-link>
+    <router-link to="/search"></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+}
+</script>
+
+<style lang='stylus' type='text/stylus' scoped>
+
+</style>
