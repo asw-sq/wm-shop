6a0ff65400c8ebc48c775e4fbaac4697e168b5b6
asw-sq <58130503@qq.com>
2020-09-22 12:32:18 +0800

    你好

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+  我更改了我的文件
       <router-link to="/msite">导航1</router-link> 
       <router-link to="/order">导航2</router-link> 
       <router-link to="/profile">导航3</router-link> 
